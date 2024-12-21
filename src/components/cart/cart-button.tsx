0bb317e9906b0184efbec47b6965979b719ba42f
theHoracle <princemjames2@gmail.com>
2024-12-21 10:52:20 +0100

    fix

--- src/components/cart/cart-button.tsx
+++ src/components/cart/cart-button.tsx
@@ -16,12 +16,14 @@ export function CartButton() {
     <SheetTrigger asChild
       className="relative p-2 text-gray-600 hover:text-gray-900"
     >
+      <>
       <ShoppingCart size={24} />
       {totalCartItems > 0 && (
         <span className="absolute top-0 right-0 -mt-1 -mr-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
           {totalCartItems}
         </span>
       )}
+      </>
     </SheetTrigger>
     <Cart
       cart={cart}
